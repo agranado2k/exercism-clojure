9f1ef8fb22129b42f5404184efaa554746ca9e0b
Arthur Granado <agranado@pluga.co>
2017-07-08 12:59:07 -0300

    Clojure - word-count i3

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -2,21 +2,11 @@
   (:require [clojure.string :as string])
   )
 
-(defn recreate-r
-  [r w]
-  (assoc r w (inc (or (get r w) 0)))
-)
-
 (defn word-count
   [phrase]
-   (let [words (re-seq #"\w+|\d+" (string/lower-case phrase))
-         result {}
-         ]
-      (loop [w words r result]
-        (if (empty? w)
-          r
-          (recur (rest w) (recreate-r r (first w)))
-        )
-      )
-    )
+  (reduce
+     #(assoc %1 %2 (inc (or (get %1 %2) 0)))
+     {}
+     (re-seq #"\w+|\d+" (string/lower-case phrase))
+  )
 )
